8417742cb7e2ab9ff412747f0dd1c4296c270237
Howard Richards <dev@conficient.com>
2018-06-16 17:24:23 +0100

    Amended to take arguments for server and client name

--- PingSource/Program.cs
+++ PingSource/Program.cs
@@ -6,9 +6,27 @@ namespace PingSource
 {
     class Program
     {
+
+        private static string _serverUrl;
+
+        private static string _clientName;
+
         static async Task Main(string[] args)
         {
-            Console.WriteLine("This is the ping source console app");
+            Console.WriteLine("PingSource console app");
+
+            if(args.Length != 2)
+            {
+                Console.WriteLine("Invalid arguments. Usage:");
+                Console.WriteLine("pingsource.exe [serverURL] [clientName]");
+                Console.ReadKey();
+                return;
+            }
+
+            // save args
+            _serverUrl = args[0].TrimEnd('/');
+            _clientName = args[1];
+
             Console.WriteLine("(waiting 3 seconds for server to load)");
             await Task.Delay(3000);
 
@@ -27,7 +45,7 @@ namespace PingSource
         private static async Task RunSignalRasync()
         {
 
-            const string hubUrl = "http://localhost:25869/TestHub";
+            string hubUrl = $"{_serverUrl}/TestHub";
             // register the source with the SignalR hub and start sending a ping out
             // at the set interval
             var connection = new HubConnectionBuilder()
@@ -49,12 +67,11 @@ namespace PingSource
                 Console.WriteLine("\nType a message or just a blank line to stop");
                 do
                 {
-                    Console.Write("message: ");
                     var input = Console.ReadLine();
                     if (string.IsNullOrEmpty(input))
                         break;
                     // send a test message
-                    await connection.InvokeAsync("SendMessage", "consoleapp", input);
+                    await connection.InvokeAsync("SendMessage", _clientName, input);
 
                 } while (true);
             }
